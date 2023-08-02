e97a497a97ab26372ec41966c417665cfe091fdf
Your Name <you@example.com>
2023-08-02 10:09:33 +0900

    데이터 넘기는걸 까먹었네

--- kingchart.js
+++ kingchart.js
@@ -1,9 +1,9 @@
-function barChart(mychart_id){
+function barChart(mychart_id,data) {
     const mychart = document.getElementById(mychart_id);
     mychart.style.display = "flex";
 
     //데이터 전처리
-    var maxValue = Math.max(...data) + 10; // Math.max(...data)로 수정하여 최대값 계산
+    var maxValue = Math.max(...data.data) + 10; // Math.max(...data.data)로 수정하여 최대값 계산
     var barWidth = 40; // 각 막대의 너비를 40px로 설정
     var gap = 20; // 막대 간 간격을 20px로 설정
     var bottomMargin = 55; // 막대 차트 하단 여백을 70px로 설정
@@ -19,11 +19,11 @@ function barChart(mychart_id){
     //캔버스 만들어서 div에 넣기
     let scrollDiv = createObject(mychart, 'div')
     scrollDiv.style.width = mychart.offsetWidth + "px";
-    scrollDiv.style.height = mychart.offsetHeight+ "px";
+    scrollDiv.style.height = mychart.offsetHeight + "px";
     scrollDiv.style.overflow = "hidden"; // 스크롤 기능 활성화
 
     let canvas = createObject(scrollDiv, 'canvas');
-    canvas.width = data.length * 60; // 막대 간 간격 60px로 설정
+    canvas.width = data.data.length * 60; // 막대 간 간격 60px로 설정
     canvas.height = mychart.offsetHeight - (yPadding - 9);
     canvas.style.background = "transparent"
 
@@ -43,20 +43,20 @@ function barChart(mychart_id){
         cty.fillStyle = '#000000';
         var yAxisLabel = yAxisLabelInterval * i;
         var yPosition = canvasHeight - (yAxisLabel / maxValue) * canvasHeight + yPadding; // 라벨의 위치 설정
-        cty.fillText(yAxisLabel, Ycanvas.width - 25 , yPosition); // y축 라벨링
+        cty.fillText(yAxisLabel, Ycanvas.width - 25, yPosition); // y축 라벨링
 
         // y축 라벨링을 위한 점선 그리기 (가로로 변경)
-        if(i == 0){
+        if (i == 0) {
             ctx.beginPath();
-            ctx.moveTo(0, yPosition+1); // 실선 시작점
-            ctx.lineTo(canvas.width, yPosition+1); // 점선 끝점
+            ctx.moveTo(0, yPosition + 1); // 실선 시작점
+            ctx.lineTo(canvas.width, yPosition + 1); // 점선 끝점
             ctx.stroke();
-            
+
             cty.beginPath();
             cty.moveTo(Ycanvas.width - 1, yPadding - 10); // 실선 시작점
-            cty.lineTo(Ycanvas.width - 1, yPosition+2); // 점선 끝점
+            cty.lineTo(Ycanvas.width - 1, yPosition + 2); // 점선 끝점
             cty.stroke();
-        }else{
+        } else {
             ctx.beginPath();
             ctx.lineWidth = 1;
             ctx.setLineDash([2, 2]);
@@ -65,23 +65,23 @@ function barChart(mychart_id){
             ctx.stroke();
         }
     }
-    
-    for (var i = 0; i < data.length; i++) {
-        var barHeight = (data[i] / maxValue) * canvasHeight;
-        var y = canvasHeight - barHeight;
-        ctx.fillStyle = colors[i % colors.length]; // colors 배열에서 색상을 선택할 때 인덱스 오버플로우 방지
 
-        ctx.fillRect(x, y+yPadding, barWidth, barHeight);
+    for (var i = 0; i < data.data.length; i++) {
+        var barHeight = (data.data[i] / maxValue) * canvasHeight;
+        var y = canvasHeight - barHeight;
+        ctx.fillStyle = data.colors[i % data.colors.length]; // colors 배열에서 색상을 선택할 때 인덱스 오버플로우 방지
+
+        ctx.fillRect(x, y + yPadding, barWidth, barHeight);
 
         ctx.fillStyle = '#000000';
         ctx.font = '14px Arial';
         ctx.textAlign = 'center'; // 텍스트 가운데 정렬
 
         // x축 라벨링
-        ctx.fillText(labels[i], x + barWidth / 2, canvasHeight + 20 + yPadding);
+        ctx.fillText(data.labels[i], x + barWidth / 2, canvasHeight + 20 + yPadding);
 
         // 바 위에 바의 값을 표시 (값이 보이도록 위치 조정)
-        ctx.fillText(data[i], x + barWidth / 2, y - 5 + yPadding);
+        ctx.fillText(data.data[i], x + barWidth / 2, y - 5 + yPadding);
 
         x += barWidth + gap;
     }
@@ -93,7 +93,7 @@ function barChart(mychart_id){
 //Object라고 적긴했지만 엘리먼트임
 //매개변수 : 추가할 부모 엘리먼트,만들고 싶은 엘리먼트
 //반환 : 만들어진 엘리먼트 객체
-function createObject(parent, mode){
+function createObject(parent, mode) {
     let object = document.createElement(mode);
     parent.appendChild(object);
 
@@ -103,7 +103,7 @@ function createObject(parent, mode){
 
 //마우스 클릭 스크롤 함수
 //매개변수 : 엘리먼트객체
-function mousescroll(Object){
+function mousescroll(Object) {
     let isDragging = false;
     let dragStartX, scrollStartX;
     let dragStartY, scrollStartY;
@@ -134,4 +134,50 @@ function mousescroll(Object){
             Object.scrollTop = scrollStartY - deltaY; // 마우스 드래그에 따라 스크롤 기능 적용
         }
     });
+}
+
+function graphChart(mychart_id) {
+    // 캔버스 요소 가져오기
+    const canvas = document.getElementById('graphCanvas');
+    const ctx = canvas.getContext('2d');
+
+    // 데이터 배열 (가상의 데이터 예시)
+    const data = [10, 20, 30, 25, 40, 15];
+
+    // 그래프의 가로, 세로 크기 가져오기
+    const canvasWidth = canvas.width;
+    const canvasHeight = canvas.height;
+
+    // 그래프의 간격과 여백 설정
+    const padding = 20;
+    const graphWidth = canvasWidth - 2 * padding;
+    const graphHeight = canvasHeight - 2 * padding;
+
+    // x축 간격 계산
+    const dataCount = data.length;
+    const xInterval = graphWidth / (dataCount - 1);
+
+    // y축 최댓값, 최솟값 계산
+    const maxValue = Math.max(...data);
+    const minValue = Math.min(...data);
+
+    // y축 간격 계산
+    const yInterval = graphHeight / (maxValue - minValue);
+
+    // 그래프 그리기 시작
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    ctx.beginPath();
+    ctx.strokeStyle = 'blue';
+    ctx.moveTo(padding, canvasHeight - padding - (data[0] - minValue) * yInterval);
+
+    // 데이터를 이용하여 그래프 그리기
+    for (let i = 1; i < dataCount; i++) {
+        const x = padding + i * xInterval;
+        const y = canvasHeight - padding - (data[i] - minValue) * yInterval;
+        ctx.lineTo(x, y);
+    }
+
+    ctx.stroke();
+    ctx.closePath();
 }
